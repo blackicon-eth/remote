b949436cda6517d35e425703dd192ed5bc35438a
Blackicon <mattiaverdecchi@yahoo.it>
2025-05-30 22:22:06 +0200

    Added new buttons for UI testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
 "use client";
 
 import { useOpportunities } from "@/components/context/opportunities-provider";
+import { ChainButton } from "@/components/custom-ui/chain-button";
 import { FullPageError } from "@/components/custom-ui/fullpage-error";
 import { FullPageLoading } from "@/components/custom-ui/fullpage-loading";
+import { ModeButton } from "@/components/custom-ui/mode-button";
+import { ListModes, SupportedNetworks } from "@/lib/enums";
+import { ChainColors, ChainImages } from "@/lib/constants";
 import { AnimatePresence, motion } from "motion/react";
+import { useState } from "react";
 
 export default function Home() {
+  const [selectedChains, setSelectedChains] = useState<SupportedNetworks[]>([
+    SupportedNetworks.ARBITRUM,
+    SupportedNetworks.BASE,
+    SupportedNetworks.POLYGON,
+  ]);
+  const [selectedMode, setSelectedMode] = useState<ListModes>(
+    ListModes.ALL_POSITIONS
+  );
+
   const {
-    opportunities,
+    //opportunities,
     isLoadingOpportunities,
     isErrorOpportunities,
     refetchOpportunities,
@@ -33,11 +47,46 @@ export default function Home() {
             Stake in one Transaction. Remotely.
           </h2>
 
-          {/* Opportunities */}
-          <div className="flex flex-col items-center justify-start gap-4">
-            {opportunities?.map((opportunity) => (
-              <div key={opportunity.key}>{opportunity.name}</div>
-            ))}
+          <div className="flex flex-col justify-start items-start w-full h-full gap-4">
+            {/* Filters */}
+            <div className="flex justify-between items-center w-full gap-4 px-0.5">
+              <div className="flex justify-start items-center gap-4">
+                {Object.values(SupportedNetworks).map((chain, index) => (
+                  <ChainButton
+                    key={chain}
+                    glowColor={ChainColors[chain]}
+                    borderColor={ChainColors[chain]}
+                    chain={chain}
+                    text={chain.slice(0, 1).toUpperCase() + chain.slice(1)}
+                    imageUrl={ChainImages[chain]}
+                    alt={chain}
+                    selectedChains={selectedChains}
+                    setSelectedChains={setSelectedChains}
+                    index={index}
+                  />
+                ))}
+              </div>
+              <div className="flex justify-start items-center gap-4">
+                <ModeButton
+                  glowColor="#f5c753"
+                  borderColor="#f5c753"
+                  text="All"
+                  selectedMode={selectedMode}
+                  setSelectedMode={setSelectedMode}
+                  listMode={ListModes.ALL_POSITIONS}
+                  index={3}
+                />
+                <ModeButton
+                  glowColor="#edd072"
+                  borderColor="#edd072"
+                  text="My Positions"
+                  selectedMode={selectedMode}
+                  setSelectedMode={setSelectedMode}
+                  listMode={ListModes.MY_POSITIONS}
+                  index={4}
+                />
+              </div>
+            </div>
           </div>
         </motion.div>
       )}
