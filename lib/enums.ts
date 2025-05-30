ca77ccd8c0d1bc0e628c516bc6e6f0549a83d029
Blackicon <mattiaverdecchi@yahoo.it>
2025-05-30 21:34:52 +0200

    Created enums

---
+++ lib/enums.ts
@@ -0,0 +1,25 @@
+// Supported Networks
+export enum SupportedNetworks {
+  BASE = "base",
+  ARBITRUM = "arbitrum",
+  POLYGON = "polygon",
+}
+
+// A list of modes for the list of positions
+export enum ListModes {
+  MY_POSITIONS = "my-positions",
+  ALL_POSITIONS = "all-positions",
+}
+
+// Alchemy RPC Base URLs
+export enum AlchemyRpcBaseUrls {
+  "ethereum" = "https://eth-mainnet.g.alchemy.com/v2",
+  "op mainnet" = "https://opt-mainnet.g.alchemy.com/v2",
+  "polygon" = "https://polygon-mainnet.g.alchemy.com/v2",
+  "base" = "https://base-mainnet.g.alchemy.com/v2",
+  "arbitrum one" = "https://arb-mainnet.g.alchemy.com/v2",
+  "unichain" = "https://unichain-mainnet.g.alchemy.com/v2",
+  "celo" = "https://celo-mainnet.g.alchemy.com/v2",
+  "ink" = "https://ink-mainnet.g.alchemy.com/v2",
+  "flow evm mainnet" = "https://flow-mainnet.g.alchemy.com/v2",
+}
