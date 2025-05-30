807704c7b2f84b330822d8fe3f4fd2a2419ca749
Blackicon <mattiaverdecchi@yahoo.it>
2025-05-30 23:11:10 +0200

    Created a table to show opportunities; Created logic to filter opportunities

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,10 @@ import { ModeButton } from "@/components/custom-ui/mode-button";
 import { ListModes, SupportedNetworks } from "@/lib/enums";
 import { ChainColors, ChainImages } from "@/lib/constants";
 import { AnimatePresence, motion } from "motion/react";
-import { useState } from "react";
+import { useState, useMemo, useEffect, useCallback } from "react";
+import { PlaceholdersAndVanishInput } from "@/components/aceternity-ui/placeholder-vanish-input";
+import { ChevronDown } from "lucide-react";
+import { useCart } from "@/components/context/cart-provider";
 
 export default function Home() {
   const [selectedChains, setSelectedChains] = useState<SupportedNetworks[]>([
@@ -19,14 +22,25 @@ export default function Home() {
   const [selectedMode, setSelectedMode] = useState<ListModes>(
     ListModes.ALL_POSITIONS
   );
+  const [searchInput, setSearchInput] = useState<string>("");
 
   const {
-    //opportunities,
+    opportunities,
     isLoadingOpportunities,
     isErrorOpportunities,
     refetchOpportunities,
     isRefetchingOpportunities,
   } = useOpportunities();
+
+  const filteredOpportunities = useMemo(() => {
+    return opportunities?.filter((opportunity) =>
+      opportunity.name.toLowerCase().includes(searchInput.toLowerCase())
+    );
+  }, [opportunities, searchInput]);
+
+  useEffect(() => {
+    console.log(opportunities);
+  }, [opportunities]);
 
   return (
     <AnimatePresence mode="wait">
@@ -85,6 +99,69 @@ export default function Home() {
                   listMode={ListModes.MY_POSITIONS}
                   index={4}
                 />
+              </div>
+            </div>
+
+            {/* Table */}
+            <div className="flex flex-col justify-start items-start size-full rounded-lg bg-neutral-900/50 border border-neutral-800 pt-2 gap-2 z-[1]">
+              {/* Table header */}
+              <div className="flex justify-between items-center w-full h-full px-2">
+                <div className="flex justify-start items-center shrink-0 w-[50%]">
+                  <PlaceholdersAndVanishInput
+                    placeholders={[
+                      "USDT...",
+                      "Curve...",
+                      "Aave...",
+                      "Uniswap...",
+                    ]}
+                    onChange={(e) => setSearchInput(e.target.value)}
+                    setSearchInput={setSearchInput}
+                  />
+                </div>
+                <div className="flex justify-between items-center shrink-0 w-[500px] h-[40px] text-white text-sm pr-[44px]">
+                  <button className="flex justify-center items-center w-[25%] h-full">
+                    Deposited
+                  </button>
+                  <button className="flex justify-center items-center gap-1 w-[25%] h-full cursor-pointer hover:text-neutral-300 transition-colors">
+                    Current APY
+                  </button>
+                  <button className="flex justify-center items-center gap-1 w-[25%] h-full cursor-pointer hover:text-neutral-300 transition-colors">
+                    Liquidity
+                  </button>
+                </div>
+              </div>
+
+              {/* Table body */}
+              <div className="flex flex-col rounded-b-lg bg-neutral-800/50 h-[460px] overflow-y-auto w-full [&::-webkit-scrollbar]:w-1 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:bg-neutral-600 [&::-webkit-scrollbar-thumb]:rounded-full">
+                <AnimatePresence mode="wait">
+                  {filteredOpportunities?.length === 0 ? (
+                    <motion.div
+                      key="no-results"
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                      className="flex justify-center items-center w-full h-full"
+                    >
+                      <div className="text-white text-sm">
+                        No results found :(
+                      </div>
+                    </motion.div>
+                  ) : (
+                    <motion.div
+                      key="opportunities"
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      exit={{ opacity: 0 }}
+                      className="flex flex-col justify-start items-start w-full h-full"
+                    >
+                      <AnimatePresence>
+                        {filteredOpportunities?.map((opportunity, index) => (
+                          <p key={index}>{opportunity.name}</p>
+                        ))}
+                      </AnimatePresence>
+                    </motion.div>
+                  )}
+                </AnimatePresence>
               </div>
             </div>
           </div>
